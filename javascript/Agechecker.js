b7c31e1cbd5647736a764dedf83a6021bb61899f
Jeff Abuan <140471708+Fniche05@users.noreply.github.com>
2023-10-28 10:59:27 +0800

    this is age checker

---
+++ javascript/Agechecker.js
@@ -0,0 +1,7 @@
+let age = prompt('Input your age')
+
+If(age >= 0){
+    console.log('The age is Positive')
+} else{
+    console.log('Error')
+}
---
+++ javascript/adultchecker.js
@@ -0,0 +1,7 @@
+let age = prompt('Input your age')
+
+If(age >= 18){
+    console.log("You're Adult")
+} else{
+    console.log('You're a minor')
+}
